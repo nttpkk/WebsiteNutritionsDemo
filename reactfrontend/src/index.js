80348f95ac0b57288649ae6e4d7d4c23fb113e9a
nttpkk <antti.virranniemi@gmail.com>
2019-03-09 21:50:39 +0200

    Index.js modifed.

--- reactfrontend/src/index.js
+++ reactfrontend/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Userlist from './Userlist';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        {/* <App /> */}
+        <Userlist/>
+    </div>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
