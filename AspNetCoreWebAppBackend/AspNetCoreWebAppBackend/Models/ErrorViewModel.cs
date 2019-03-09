b13885f1008211654c562df9aab1059d1cfed664
nttpkk <antti.virranniemi@gmail.com>
2019-03-09 20:06:49 +0200

    Gitignore modified.

---
+++ AspNetCoreWebAppBackend/AspNetCoreWebAppBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreWebAppBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
